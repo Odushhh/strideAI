33a9010e4a1395679233a8b8818064eb090058ce
Adrian Oduma <98812231+Odushhh@users.noreply.github.com>
2025-03-20 20:48:47 +0300

    Add GA tag

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -23,6 +23,14 @@ const Index = () => {
       </main>
       <Footer />
     </div>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-L275T6WE5D"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-L275T6WE5D');
+    </script>
   );
 };
 
